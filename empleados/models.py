056b17437dfbaa93054230d955c9987b2211b9ec
uanmita <uanmita@gmail.com>
2025-07-18 12:45:50 +0200

    Models_creations

--- empleados/models.py
+++ empleados/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Departamento(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
