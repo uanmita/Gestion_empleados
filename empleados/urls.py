b2fbe2b6222d4584cc2b779966cecae66016fa80
uanmita <uanmita@gmail.com>
2025-07-19 10:55:00 +0200

    Modificacion de aspecto general y urls actualizado

--- empleados/urls.py
+++ empleados/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-      path('empleados/', views.empleados_list, name='empleados'),
+    path('empleados/', views.empleados_list, name='empleados'),
+    path('nuevo/', views.empleados_create, name='nuevo'),
+    path('', views.index, name='index'),
 ]
--- empleados/views.py
+++ empleados/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Empleado
 
+def index(request):
+    return render(request, 'empleados/index.html')
 def empleados_list(request):
     empleados = Empleado.objects.select_related('departamento').all()  # Agregué .all()
-    return render(request, 'empleados/index.html', {'empleados': empleados})
+    return render(request, 'empleados/list.html', {'empleados': empleados})
+
+def empleados_create(request):
+   return render(request, 'empleados/new.html')
