108af3549d55b48608349c64f1256e0afc3a8bce
uanmita <uanmita@gmail.com>
2025-07-19 11:56:45 +0200

    Creacion de portfolio.html

--- empleados/urls.py
+++ empleados/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('empleados/', views.empleados_list, name='empleados'),
     path('nuevo/', views.empleados_create, name='nuevo'),
+    path('portfolio/', views.portfolio_list, name='portfolio'),
     path('', views.index, name='index'),
 ]
--- empleados/views.py
+++ empleados/views.py
@@ -3,9 +3,13 @@ from .models import Empleado
 
 def index(request):
     return render(request, 'empleados/index.html')
+
 def empleados_list(request):
     empleados = Empleado.objects.select_related('departamento').all()  # Agregué .all()
     return render(request, 'empleados/list.html', {'empleados': empleados})
 
 def empleados_create(request):
    return render(request, 'empleados/new.html')
+
+def portfolio_list(request):
+    return render(request, 'empleados/portfolio.html')
